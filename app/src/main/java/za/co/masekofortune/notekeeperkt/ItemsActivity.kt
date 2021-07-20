f3f2c38157348705673ba8be139764a85935c62e
Fortune Maseko <mrfortune.maseko@gmail.com>
2021-07-20 14:10:12 +0200

    Add course adapter

---
+++ app/src/main/java/za/co/masekofortune/notekeeperkt/ItemsActivity.kt
@@ -0,0 +1,132 @@
+package za.co.masekofortune.notekeeperkt
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import za.co.masekofortune.notekeeperkt.databinding.ActivityItemsBinding
+
+class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityItemsBinding
+
+    private val noteLayoutManager by lazy { LinearLayoutManager(this) }
+    private val courseLayoutManager by lazy { GridLayoutManager(this, 2) }
+    private val noteRecyclerAdapter by lazy { NoteRecyclerAdapter(this, DataManager.notes) }
+    private val courseRecyclerAdapter by lazy { CourseRecyclerAdapter(this, DataManager.courses.values.toList()) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityItemsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarItems.toolbar)
+
+        binding.appBarItems.fab.setOnClickListener { view ->
+            val activityIntent = Intent(this, NoteActivity::class.java)
+            startActivity(activityIntent)
+        }
+
+        displayNotes()
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            binding.appBarItems.toolbar,
+            R.string.open_nav_drawer,
+            R.string.close_nav_drawer
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun displayNotes() {
+        val listItems = binding.appBarItems.contentItems.listItems
+        listItems.layoutManager = noteLayoutManager
+        listItems.adapter = noteRecyclerAdapter
+
+        binding.navView.menu.findItem(R.id.nav_notes).isChecked = true
+    }
+
+    private fun displayCourses() {
+        val listItems = binding.appBarItems.contentItems.listItems
+        listItems.layoutManager = courseLayoutManager
+        listItems.adapter = courseRecyclerAdapter
+
+        binding.navView.menu.findItem(R.id.nav_courses).isChecked = true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.items, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_courses -> {
+                displayCourses()
+            }
+
+            R.id.nav_notes -> {
+                displayNotes()
+            }
+
+            R.id.nav_share -> {
+                handleSelection("Don't you think you've shared enough")
+            }
+
+            R.id.nav_send -> {
+                handleSelection("Send")
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun handleSelection(message: String) {
+        Snackbar.make(binding.appBarItems.contentItems.listItems, message, Snackbar.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.appBarItems.contentItems.listItems.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
